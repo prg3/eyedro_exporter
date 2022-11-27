031c76e36c77e471e7d66b6aa60f5a05cec46eb2
Paul Greidanus <paul@majestik.org>
2022-11-27 10:00:36 -0700

    initial

---
+++ exporter.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"reflect"
+	"fmt"
+)
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module exporter/eyedro
+
+go 1.17
